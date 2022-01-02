efe2d98ed8debe870ec624174887fd537d0a28e3
kaigedong <dongkaige@gmail.com>
2022-01-02 11:16:54 +0800

    update github action

--- update_m3u.sh
+++ update_m3u.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 wget https://raw.githubusercontent.com/iptv-org/iptv/master/channels/cn.m3u 2>/dev/null
-echo "#EXTM3U" > ./source_cn_1080/cn_1080.m3u
-grep -A 1 "\(1080p\)" cn.m3u | grep -v '^--' >> ./source_cn_1080/cn_1080.m3u
+echo "#EXTM3U" > source_cn_1080/cn_1080.m3u
+grep -A 1 "\(1080p\)" cn.m3u | grep -v '^--' >> source_cn_1080/cn_1080.m3u
 
