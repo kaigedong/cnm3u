fb38ca21ab2652c2bb6464e0600f95714fc1ad5f
kaigedong <dongkaige@gmail.com>
2022-01-03 15:50:04 +0800

    增加白名单排序

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # 使用说明:
 
-每天自动根据上游仓库`https://github.com/iptv-org/iptv`获取最新播放源，并按规则进行筛选:
+每天自动获取上游仓库`https://github.com/iptv-org/iptv`的`中国源`，并按规则进行筛选:
 
 - 首先筛选掉黑名单`blacklist.txt`
 
 - 然后导出白名单里的列表`whitelist.txt`
+
+- [x] 列表顺序按照白名单顺序
 
 ```
 # m3u链接
--- filter_m3u.py
+++ filter_m3u.py
@@ -1,5 +1,6 @@
+from collections import OrderedDict
 
-whiteList = {}
+whiteList = OrderedDict()
 blackList = {}
 
 # 读取白名单
@@ -7,7 +8,7 @@ with open("whitelist.txt", 'r') as fin:
     for aline in fin:
         aline = aline.strip()
         if aline != "":
-            whiteList[aline] = 1
+            whiteList[aline] = []
 # 读取黑名单
 with open("blacklist.txt", "r") as fin:
     for aline in fin:
@@ -16,11 +17,19 @@ with open("blacklist.txt", "r") as fin:
             blackList[aline] = 1
 
 
-def isInWhite(aStr):
+def isInWhite(aStr, nextStr):
     for aItem in whiteList:
         if aItem in aStr:
+            whiteList[aItem].append([aStr, nextStr])
             return True
     return False
+
+
+def printWhite():
+    for aWhite in whiteList:
+        for aItem in whiteList[aWhite]:
+            print(aItem[0])
+            print(aItem[1])
 
 
 def isInBlack(aStr):
@@ -41,8 +50,9 @@ with open("cn.m3u", 'r') as fin:
         if aLine.startswith("#"):
             if isInBlack(aLine):
                 pass
-            elif isInWhite(aLine):
-                print(aLine)
-                print(nextLine)
+            elif isInWhite(aLine, nextLine):
+                pass
         # 避免有多个注释行
         index += 1
+
+printWhite()
