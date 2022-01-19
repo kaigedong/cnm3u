060edb2e337b264d0165425590528a1649d8b4b7
kaigedong <dongkaige@gmail.com>
2022-01-19 18:40:51 +0800

    添加4k source

--- update_m3u.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://iptv-org.github.io/iptv/countries/cn.m3u 2>/dev/null -O cn.m3u
-echo "#EXTM3U" > cn_1080/cn1080.m3u
-grep -A 1 --no-group-separator -F "(1080p)" cn.m3u >> cn_1080/cn1080.m3u
-
